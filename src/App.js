0c266c963c255a8c4590c49f328f6d87f9954a34
Anand Kumar <anand.sharma0510@gmail.com>
2020-09-19 21:56:10 +0530

    fixed the laylout issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Card from './card/index'
+import Card from './card'
 
 function App() {
   return (
--- src/card/card.js
+++ src/card/card.js
@@ -2,177 +2,131 @@ import React from "react";
 import PropTypes from "prop-types";
 import _ from "lodash";
 import { Responsive, WidthProvider } from "react-grid-layout";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 export default class ShowcaseLayout extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentBreakpoint: "lg",
-            compactType: "vertical",
-            mounted: false,
-            layouts: { lg: props.initialLayout }
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentBreakpoint: "lg",
+      compactType: "vertical",
+      mounted: false,
+      layouts: { lg: props.initialLayout }
+    };
 
-        this.onBreakpointChange = this.onBreakpointChange.bind(this);
-        this.onCompactTypeChange = this.onCompactTypeChange.bind(this);
-        this.onLayoutChange = this.onLayoutChange.bind(this);
-        this.onNewLayout = this.onNewLayout.bind(this);
-    }
+    this.onBreakpointChange = this.onBreakpointChange.bind(this);
+    this.onCompactTypeChange = this.onCompactTypeChange.bind(this);
+    this.onLayoutChange = this.onLayoutChange.bind(this);
+    this.onNewLayout = this.onNewLayout.bind(this);
+  }
 
-    componentDidMount() {
-        this.setState({ mounted: true });
-    }
+  componentDidMount() {
+    this.setState({ mounted: true });
+  }
 
-    generateDOM() {
-        return _.map(this.state.layouts.lg, function (l, i) {
-            return (
-                <div key={i} style={{ border: '2px black', backgroundColor: 'blue' }} className={l.static ? "static" : ""}>
-                    {l.static ? (
-                        <span
-                            className="text"
-                            title="This item is static and cannot be removed or resized."
-                        >
-                            Static - {i}
-                        </span>
-                    ) : (
-                            <div>
-                                <Table aria-label="simple table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell>Dessert (100g serving)</TableCell>
-                                            <TableCell align="right">Calories</TableCell>
-                                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-
-                                        <TableRow key={i}>
-                                            <TableCell component="th" scope="row">
-                                                {'row.name'}
-                                            </TableCell>
-                                            <TableCell align="right">{'row.calories'}</TableCell>
-                                            <TableCell align="right">{'row.fat'}</TableCell>
-                                            <TableCell align="right">{'row.carbs'}</TableCell>
-                                            <TableCell align="right">{'row.protein'}</TableCell>
-                                        </TableRow>
-
-                                    </TableBody>
-                                </Table>
-                            </div>
-                        )}
-                </div>
-            );
-        });
-    }
-
-    onBreakpointChange(breakpoint) {
-        this.setState({
-            currentBreakpoint: breakpoint
-        });
-    }
-
-    onCompactTypeChange() {
-        const { compactType: oldCompactType } = this.state;
-        const compactType =
-            oldCompactType === "horizontal"
-                ? "vertical"
-                : oldCompactType === "vertical"
-                    ? null
-                    : "horizontal";
-        this.setState({ compactType });
-    }
-
-    onLayoutChange(layout, layouts) {
-        this.props.onLayoutChange(layout, layouts);
-    }
-
-    onNewLayout() {
-        this.setState({
-            layouts: { lg: generateLayout() }
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <div>
-                    Current Breakpoint: {this.state.currentBreakpoint} ({
-                        this.props.cols[this.state.currentBreakpoint]
-                    }{" "}
-          columns)
+  generateDOM() {
+    return _.map(this.state.layouts.lg, function (l, i) {
+      return (
+        <div key={i} className={l.static ? "static" : ""}>
+          {l.static ? (
+            <span
+              className="text"
+              title="This item is static and cannot be removed or resized."
+            >
+              Static - {i}
+            </span>
+          ) : (
+            <span className="text">{i}</span>
+          )}
         </div>
-                <div>
-                    Compaction type:{" "}
-                    {_.capitalize(this.state.compactType) || "No Compaction"}
-                </div>
-                <button onClick={this.onNewLayout}>Generate New Layout</button>
-                <button onClick={this.onCompactTypeChange}>
-                    Change Compaction Type
+      );
+    });
+  }
+
+  onBreakpointChange(breakpoint) {
+    this.setState({
+      currentBreakpoint: breakpoint
+    });
+  }
+
+  onCompactTypeChange() {
+    const { compactType: oldCompactType } = this.state;
+    const compactType =
+      oldCompactType === "horizontal"
+        ? "vertical"
+        : oldCompactType === "vertical"
+        ? null
+        : "horizontal";
+    this.setState({ compactType });
+  }
+
+  onLayoutChange(layout, layouts) {
+    this.props.onLayoutChange(layout, layouts);
+  }
+
+  onNewLayout() {
+    this.setState({
+      layouts: { lg: generateLayout() }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          Current Breakpoint: {this.state.currentBreakpoint} (
+          {this.props.cols[this.state.currentBreakpoint]} columns)
+        </div>
+        <div>
+          Compaction type:{" "}
+          {_.capitalize(this.state.compactType) || "No Compaction"}
+        </div>
+        <button onClick={this.onNewLayout}>Generate New Layout</button>
+        <button onClick={this.onCompactTypeChange}>
+          Change Compaction Type
         </button>
-                <ResponsiveReactGridLayout
-                    {...this.props}
-                    layouts={this.state.layouts}
-                    onBreakpointChange={this.onBreakpointChange}
-                    onLayoutChange={this.onLayoutChange}
-                    // WidthProvider option
-                    measureBeforeMount={false}
-                    // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
-                    // and set `measureBeforeMount={true}`.
-                    useCSSTransforms={this.state.mounted}
-                    compactType={this.state.compactType}
-                    preventCollision={!this.state.compactType}
-                >
-                    {this.generateDOM()}
-                </ResponsiveReactGridLayout>
-            </div>
-        );
-    }
+        <ResponsiveReactGridLayout
+          {...this.props}
+          layouts={this.state.layouts}
+          onBreakpointChange={this.onBreakpointChange}
+          onLayoutChange={this.onLayoutChange}
+          // WidthProvider option
+          measureBeforeMount={false}
+          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
+          // and set `measureBeforeMount={true}`.
+          useCSSTransforms={this.state.mounted}
+          compactType={this.state.compactType}
+          preventCollision={!this.state.compactType}
+        >
+          {this.generateDOM()}
+        </ResponsiveReactGridLayout>
+      </div>
+    );
+  }
 }
 
 ShowcaseLayout.propTypes = {
-    onLayoutChange: PropTypes.func.isRequired
+  onLayoutChange: PropTypes.func.isRequired
 };
 
 ShowcaseLayout.defaultProps = {
-    className: "layout",
-    rowHeight: 200,
-    rowWidth: 200,
-    onLayoutChange: function () { },
-    cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
-    initialLayout: generateLayout()
+  className: "layout",
+  rowHeight: 30,
+  onLayoutChange: function () {},
+  cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+  initialLayout: generateLayout()
 };
 
 function generateLayout() {
-    return _.map(_.range(0, 25), function (item, i) {
-        var y = Math.ceil(Math.random() * 4) + 1;
-        return {
-            x: (_.random(0, 5) * 2) % 12,
-            y: Math.floor(i / 6) * y,
-            w: 2,
-            h: y,
-            i: i.toString(),
-            static: Math.random() < 0.05
-        };
-    });
+  return _.map(_.range(0, 25), function (item, i) {
+    var y = Math.ceil(Math.random() * 4) + 1;
+    return {
+      x: (_.random(0, 5) * 2) % 12,
+      y: Math.floor(i / 6) * y,
+      w: 2,
+      h: y,
+      i: i.toString(),
+      static: Math.random() < 0.05
+    };
+  });
 }
